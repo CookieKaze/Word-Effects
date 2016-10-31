1b177d53e147eff695f93125aeb0161d4757393e
Erin Luu <luuerin@gmail.com>
2016-10-31 17:16:29 -0400

    Initial Commit

---
+++ Word Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word Effects
+//
+//  Created by Erin Luu on 2016-10-31.
+//  Copyright © 2016 Erin Luu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
